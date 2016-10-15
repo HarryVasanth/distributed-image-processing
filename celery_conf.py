979c99f0935b12f02193d17f37d1a59dc494821c
Fabio <fabiocl93@gmail.com>
2016-10-15 16:53:13 +0100

    Update celery_conf.py

--- celery_conf.py
+++ celery_conf.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from celery import Celery
 
+#TODO Change the broker for it to be able to use different machines (Virtual ones)
 app = Celery('Distributed-Systems-Image-Processing',
              backend='rpc://',
              broker='amqp://guest@localhost//',
