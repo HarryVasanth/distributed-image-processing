8929549c3a69128fbc4592065485bbbb953309d1
Vítor Baptista <vmcbaptista95@gmail.com>
2016-10-17 13:11:31 +0100

    Support to multiple workers in different machines. Creation of more than one matrix.
    Bug: Possible deadlock in the client.py

--- celery_conf.py
+++ celery_conf.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('Distributed-Systems-Image-Processing',
-             backend='rpc://',
-             broker='amqp://guest@localhost//',
+             broker='amqp://dist:dist@192.168.10.94/imageproc', # The IP should be changed!
+             backend='redis://192.168.10.170:6379/0', # The IP should be changed!
              include=['Distributed-Systems-Image-Processing.tasks'])
