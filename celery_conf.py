b019878b2f9d36daddee08d2ba986637295a328a
HarryVasanth <mailsuthan@gmail.com>
2016-11-25 12:31:39 +0000

    Image Split Done

--- celery_conf.py
+++ celery_conf.py
@@ -1,9 +1,8 @@
-from __future__ import absolute_import
-
 from celery import Celery
 
-#TODO Change the broker for it to be able to use different machines (Virtual ones)
-app = Celery('Distributed-Systems-Image-Processing',
-             broker='amqp://dist:dist@192.168.10.94/imageproc', # The IP should be changed!
-             backend='redis://192.168.10.170:6379/0', # The IP should be changed!
-             include=['Distributed-Systems-Image-Processing.tasks'])
+app = Celery('Distributed-Image-Processing-System',
+             broker='amqp://dist:dist@192.168.10.186/imageproc', # The IP shoul$
+             backend='redis://192.168.10.133:6379/0', # The IP should be change$
+             include=['client'])
+app.conf.task_serializer = 'pickle'
+app.conf.result_serializer = 'pickle'
--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import cv2 #THis will import open cv 2
-from matplotlib import pyplot as plt
 from tkinter import *
+import tasks
 
-
-
+import numpy as np
 class File():
 
     def __init__(self,path):
@@ -40,69 +39,20 @@ class File():
         Write the received matrix to a image file on the directory
         :return:
         """
+        image = np.array(image)
         dir="printado.png"
         cv2.imwrite(dir,image)
 
-
-class Algoritms():
-
-    def __init__(self):
-        pass
-
-    def edgeDetection(self, image, minVal, maxVal):
-        """
-        OpenCV functiOn that will handle all the edge detection.
-        :param image: Image path
-        :param minVal: Any edges with intensity gradient more than maxVal are sure to be edges
-        :param maxVal: Any edges below minVal are sure to be non-edges, do they're discarded
-        :return:
-        """
-        return cv2.Canny(image, minVal, maxVal)
-        # plt.subplot()
-
-
-    def imageThresholding(self, image, thresholdValue, maxVal):
-        """
-        !!IMPORTANT TO USE THIS IMAGE MUST BE IN GRAY SCALE
-        This algorithm will apply a simple threshold to the image that has been passed and will return it
-        The is a bunch different threshold types we can apply to images check
-        http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_thresholding/py_thresholding.html#thresholding
-        :param image:   Image that has been open
-        :param thresholdValue: The value fo the threshold
-        :param maxVal:  represents the value to be given if pixel value is more than (sometimes less than) the threshold value (0 - black,  255 - White)
-        :return:
-        """
-        ret, th1 = cv2.threshold(image, thresholdValue, maxVal,cv2.THRESH_BINARY)
-        return th1
-
-
-    def scaling(self):
-        pass
-
-
-    def translation(self):
-        pass
-
-    def rotation(self):
-        pass
-
-    def prespectiveTransformation(self):
-        pass
-
-    def smoothBy_Averaging(self):
-        pass
-
-    def smoothBy_Blur(self):
-        pass
-
-
-
-
-
-
-
 if __name__ == "__main__":
-    imageOpen = File("./images/x.png")
-    algoritms = Algoritms()
-    image = algoritms.imageThresholding(imageOpen.openFile(),127,122)
-    imageOpen.saveFile(image)
+    imageOpen = File("./images/section8-image.png")
+    image = imageOpen.openFile()
+    count = 0
+    task_ids = []
+    results = []
+    for chunk in image:
+        task_ids.append(tasks.imageThresholding.delay(chunk, 127, 255))
+        count += 1
+    for result in task_ids:
+        result = result.get()
+        results.append(result)
+
