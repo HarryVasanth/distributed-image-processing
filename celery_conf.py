39953633d8a70f54a11ba9a4b63d936f3647e035
HarryVasanth <mailsuthan@gmail.com>
2016-12-16 13:47:38 +0000

    Code showed on presentation

--- celery_conf.py
+++ celery_conf.py
@@ -1,8 +1,8 @@
 from celery import Celery
 
 app = Celery('Distributed-Image-Processing-System',
-             broker='amqp://dist:dist@192.168.1.103/imageproc', # The IP shoul$
-             backend='redis://192.168.1.102:6379/0', # The IP should be change$
+             broker='amqp://dist:dist@192.168.1.100/imageproc', # The IP shoul$
+             backend='redis://192.168.1.100:6379/0', # The IP should be change$
              include=['client'])
 app.conf.task_serializer = 'pickle'
 app.conf.result_serializer = 'pickle'
