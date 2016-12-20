7ec990c1fb8ff4d39807bd16141bcb8a5b09cff2
fcl <fabiocl93@gmail.com>
2016-12-20 23:12:57 +0000

    Install Celery 3.1.6 for flower api work CSV Generation

--- celery_conf.py
+++ celery_conf.py
@@ -1,8 +1,10 @@
 from celery import Celery
 
 app = Celery('Distributed-Image-Processing-System',
-             broker='amqp://dist:dist@192.168.1.103/imageproc', # The IP shoul$
-             backend='redis://192.168.1.102:6379/0', # The IP should be change$
+             broker='amqp://dist:dist@192.168.1.8/imageproc', # The IP shoul$
+             backend='redis://192.168.1.9:6379/0', # The IP should be change$
              include=['client'])
 app.conf.task_serializer = 'pickle'
 app.conf.result_serializer = 'pickle'
+app.conf.accept_content = ['pickle']
+app.conf.worker_send_task_events = 'true'
