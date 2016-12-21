f6887fb72a55d502aa5f0525e63ae0b024b2c0a3
HarryVasanth <mailsuthan@gmail.com>
2016-12-21 09:56:38 +0000

    #Bug Solved (Request the API data before the task processing finishing)

--- celery_conf.py
+++ celery_conf.py
@@ -1,8 +1,8 @@
 from celery import Celery
 
 app = Celery('Distributed-Image-Processing-System',
-             broker='amqp://dist:dist@192.168.1.8/imageproc', # The IP shoul$
-             backend='redis://192.168.1.9:6379/0', # The IP should be change$
+             broker='amqp://dist:dist@192.168.1.105/imageproc', # The IP shoul$
+             backend='redis://192.168.1.105:6379/0', # The IP should be change$
              include=['client'])
 app.conf.task_serializer = 'pickle'
 app.conf.result_serializer = 'pickle'
